2e42c83c93bc2465017f1d0ac7135e71e96f56c5
rasel440git <rasel440@gmail.com>
2023-01-30 21:31:59 +0600

    FirstAssignment

---
+++ FirstAssignment.php
@@ -0,0 +1,64 @@
+<?php
+/*
+1.	What is the Feasibility Study of the SDLC model & why is this important?
+
+Ans: A feasibility study is the initial phase of the Software Development Life Cycle that
+ evaluates the viability of a project. It determines whether the proposed project is
+ technically, economically, and legally feasible. 
+
+It is important because it helps to identify potential problems and risks early in the 
+development process, reducing the chances of project failure and saving time and resources. 
+
+
+
+
+ 
+2.	Write 5 advantages of Agile methodology?
+
+Ans:
+1.	Agile methodology allows for changes and adaptations throughout the development
+    process, making it easier to respond to shifting requirements and priorities.
+2.	Agile methodology emphasizes delivering working software quickly, enabling 
+    organizations to bring products to market faster.							
+3.	Agile methodology encourages close collaboration between development teams, 
+    stakeholders, and customers.											
+4.	Agile methodology helps ensure that the final product is of high quality and meets
+     customer requirements.												
+5.	Agile methodology puts the customer at the center of the development process, 
+    leading to increased customer engagement and satisfaction with the end product.
+
+
+
+ 
+3.	Write short notes on SDLC model phase.
+
+Ans: short notes on SDLC model phase are below: -
+
+Planning: The planning phase involves defining the project scope, goals, and objectives,
+     as well as identifying the resources and timelines needed to complete the project.
+
+Analysis: In this phase, the requirements for the project are gathered, analyzed, 
+    and documented. This information is used to create a detailed project plan.
+
+Design: The design phase involves creating a detailed design of the system, 
+    including technical specifications and architecture.
+
+Development: In this phase, the actual software is developed based on the design 
+    specifications.
+
+Testing: The testing phase involves verifying that the software meets the specified 
+    requirements and identifying and fixing any defects.
+
+Deployment: In this phase, the software is deployed into a production environment and made available 
+    to end-users.
+
+Maintenance: The maintenance phase involves ongoing support, bug fixes, and updates to the software 
+    to ensure its continued operation and performance.
+
+
+
+
+
+
+*/
+
