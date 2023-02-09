3bd2105197b69de0ea879b96034b60560ec2c8af
A K M Abdul Hai <abdul.hai@lankabd.com>
2023-02-09 20:24:23 +0600

    testfeB

--- test.php
+++ test.php
@@ -14,3 +14,6 @@ elseif($mark<=33 ){
     echo "grade is Faild."; 
 }
 echo $grade= ($mark>=60)? ($mark >=45 && $mark <=59)?($mark >33 && $mark<44)? ($mark<=33 ):"grade is Faild" :"grade is second Division": "grade is First Division";
+
+
+test lsdjksjd alkd kasdj kdj
