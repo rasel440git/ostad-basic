28722bc594afbe261235164c6d23fb9595abf9de
A K M Abdul Hai <abdul.hai@lankabd.com>
2023-02-09 20:30:50 +0600

    testFileOffice

--- test.php
+++ test.php
@@ -1,19 +1,86 @@
 <?php
-$mark=30;
-if($mark>60){
-    echo "grade is First Division";
-}
-elseif($mark >=45 && $mark <=59){
-  //elseif($mark >=45 && $mark <=59){    
-    echo "grade is second Division";
-}
-elseif($mark >33 && $mark<44){
-    echo "grade is thard Division";
-}
-elseif($mark<=33 ){
-    echo "grade is Faild."; 
-}
-echo $grade= ($mark>=60)? ($mark >=45 && $mark <=59)?($mark >33 && $mark<44)? ($mark<=33 ):"grade is Faild" :"grade is second Division": "grade is First Division";
+//  $year=1901;
+
+//  if ($year%4==0 && $year/100){
+//     echo "$year is a leaf Year";
+//  }
+//  else{
+//     echo "$year is not a leaf Year";
+//  }
+// echo "\n";
+
+ $n=14;
+//  if( $n%2==0){
+//     echo"this is an Even number";
+//  }
+//  else{
+//     echo"this is an Odd number";
+//  }
+//  echo "\n";
+
+//  echo $result= ($n%2==0) ?"this is an Even number":"this is an Odd number";
+//  echo "\n";
 
 
-test lsdjksjd alkd kasdj kdj
+ 
+// $number=10;
+// if($number<10){
+//     echo "Number is less then 10";
+// }
+// elseif($number==10){
+//     echo "Number is Equel the 10";
+// }
+// elseif($number>10){
+//     echo "Number is biger the 10";
+// }
+// else{
+//     echo "Number is unknown";
+// }
+
+// $result= ($number<10) ? "" : "";
+
+// $points = 75;
+// $grade = '';
+
+// $grade = ($points >= 90) ? 'A' :
+//          (($points >= 80) ? 'B' :
+//          (($points >= 70) ? 'C' :
+//          (($points >= 60) ? 'D' : 'F')));
+
+// echo "The student's grade is: " . $grade;
+echo PHP_EOL;
+
+
+$tutionFee=6100;
+ $commission = ($tutionFee > 20000) ? ($tutionFee * 25) / 100 :
+ (($tutionFee > 10000 && $tutionFee < 20000) ? ($tutionFee * 20) / 100 :
+  (($tutionFee > 7000 && $tutionFee < 10000) ? ($tutionFee * 15) / 100 :
+   (($tutionFee < 7000) ? "You are not eligible for commission" : "")));
+
+   echo "The commission is:- $commission"; 
+   echo PHP_EOL;
+   for($i=1; $i<10; $i++){
+       
+        echo PHP_EOL;
+        for($j=1; $j<$i; $j++)
+        echo "$j";
+        if($j==4){
+            continue;
+        }
+   }
+   echo PHP_EOL;
+   $i=1;
+   while($i<10){
+    echo "$i";
+    $i++;
+    echo PHP_EOL;
+   }
+
+   echo PHP_EOL;
+
+   $frouts= array("amm","jam","kola");
+   echo $frouts[2];
+   echo PHP_EOL;
+   foreach($frouts as $element){
+        echo $element. PHP_EOL;
+   }
