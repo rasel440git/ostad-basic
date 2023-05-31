66a78cc9e918da14e24de7002b062b0bbf08d5eb
rasel440git <rasel440@gmail.com>
2023-06-01 01:07:30 +0600

    home

--- laravel-Practic/my-app/database/migrations/2023_05_31_061945_create_users.php
+++ laravel-Practic/my-app/database/migrations/2023_05_31_061945_create_users.php
@@ -13,12 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('email',50)->unique();
+            $table->string('otp',10);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
--- laravel-Practic/my-app/database/migrations/2023_05_31_062428_create_brands.php
+++ laravel-Practic/my-app/database/migrations/2023_05_31_062428_create_brands.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('brandName',50);
+            $table->string('brandImg',300);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('brands');
     }
 };
