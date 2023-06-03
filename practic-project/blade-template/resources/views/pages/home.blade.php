0035fcd9151388c1e2bebda699e8986ace8a8a1a
rasel440git <rasel440@gmail.com>
2023-06-04 00:24:36 +0600

    home

---
+++ practic-project/blade-template/resources/views/pages/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('content')
+
+    @include('component.hero')
+    @include('component.HowWeDo')
+    @include('component.HowWeDo')
+    @include('component.priceing')
+    @include('component.team')
+
+
+
+@endsection
