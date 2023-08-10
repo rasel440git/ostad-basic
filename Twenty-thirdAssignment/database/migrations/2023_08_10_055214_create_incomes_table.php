d4f02b8fe03dfad2fe6dc37d58ab63bd66463e67
rasel440git <rasel440@gmail.com>
2023-08-11 00:10:39 +0600

    homeass

---
+++ Twenty-thirdAssignment/database/migrations/2023_08_10_055214_create_incomes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->foreignId('category_id')
+            ->constrained('categories')
+            ->cascadeOnDelete();
+            $table->string('type');
+            $table->integer('amount');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('_income_expense_details');
+    }
+};
