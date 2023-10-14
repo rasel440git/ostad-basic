51a53c52a6c26e9be89e24a8bbc1a99306455518
rasel440git <rasel440@gmail.com>
2023-10-15 00:43:50 +0600

    home

--- Twenty-fifthAssignment/routes/web.php
+++ Twenty-fifthAssignment/routes/web.php
@@ -7,9 +7,38 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+You are required to develop a fully functional payroll management software using the 
+Laravel framework. The application should have the following features: 
+
+
+User Authentication:
+
+Simple Employee Leave Application Management System
+User Type 
+
+Employees: Can request leaves.
+
+Managers: Approve or reject leave requests.
+User Type 
+Define leave categories like vacation, sick leave, etc
+
+Leave Requests:
+Employees request time off with dates and reason.
+Managers get notified of new requests.
+
+Approval Process:
+Managers approve or reject requests.
+Notifications Email sent to employees.
+
+Leave Balances:
+System tracks available leave days.
+
+Basic Reporting:
+Simple reports on leave history.
+
+Submission Instruction :
+Please submit your github repository link.
+
 |
 */
 
